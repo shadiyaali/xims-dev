87f04c6c013df01f4e960b4c51837aad9eca6eaf
shadiyaali <shadiyackn@gmail.com>
2025-04-28 16:42:01 +0530

    Save changes before rebase

--- ximspro/settings.py
+++ ximspro/settings.py
@@ -247,7 +247,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net' 
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
