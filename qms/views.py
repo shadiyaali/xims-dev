304196e934d2ee2ab2a099a8198485c8d2da5c9c
shadiyaali <shadiyackn@gmail.com>
2025-05-13 08:47:31 +0530

    baseline post add

--- qms/views.py
+++ qms/views.py
@@ -15403,7 +15403,13 @@ class BaselineView(APIView):
             "draft_records": serializer.data
         }, status=status.HTTP_200_OK)
         
-        
+    def post(self, request):
+  
+        serializer = BaselineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
         
 class EnergyImprovementsListCreateAPIView(APIView):
      
