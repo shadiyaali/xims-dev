c6313789594b177612a1963e178dd40dd8d05f2d
shadiyaali <shadiyackn@gmail.com>
2025-05-15 15:56:37 +0530

    api for view replay

--- qms/views.py
+++ qms/views.py
@@ -12876,8 +12876,33 @@ class MessageDetailView(generics.RetrieveAPIView):
             raise NotFound("Message not found.")
         
 
-
+    
+class ReplayMessageDetailView(generics.RetrieveAPIView):
+    queryset = ReplayMessage.objects.all()
+    serializer_class = ReplayMessageListSerializer
  
+    lookup_field = 'id'
+
+    def get_object(self):
+        try:
+            return ReplayMessage.objects.get(id=self.kwargs['id'])
+        except ReplayMessage.DoesNotExist:
+            raise NotFound("Message not found.")
+        
+
+
+    
+class ForwardMessageDetailView(generics.RetrieveAPIView):
+    queryset = ForwardMessage.objects.all()
+    serializer_class = ForwardMessageListSerializer
+ 
+    lookup_field = 'id'
+
+    def get_object(self):
+        try:
+            return ForwardMessage.objects.get(id=self.kwargs['id'])
+        except ForwardMessage.DoesNotExist:
+            raise NotFound("Message not found.") 
 
 class ReplyMessageCreateAPIView(APIView):
     def post(self, request, format=None):
