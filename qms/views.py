5b48294c0f5e562eb2f19745d56e48fddcdebef7
shadiyaali <shadiyackn@gmail.com>
2025-05-15 14:56:59 +0530

    update replay

--- qms/views.py
+++ qms/views.py
@@ -13112,20 +13112,31 @@ class UserInboxForwardListView(generics.ListAPIView):
     
     
 class MarkReplayMessageAsTrashView(APIView):
-
     def put(self, request, *args, **kwargs):
+        print("rrrrrrrr", request.data)
         message_id = self.kwargs.get('id')
+
         try:
-           
             message = ReplayMessage.objects.get(id=message_id)
-            message.is_trash = True
-            message.trash_user = request.user  
+
+            user_id = request.data.get('trash_user')
+            if not user_id:
+                return Response({'error': 'trash_user is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                user = Users.objects.get(id=user_id)
+            except Users.DoesNotExist:
+                return Response({'error': 'Invalid user ID provided.'}, status=status.HTTP_404_NOT_FOUND)
+
+            message.is_trash = request.data.get('is_trash', True)
+            message.trash_user = user
             message.save()
 
             return Response({
                 'status': 'success',
                 'message': 'Message marked as trash.'
             }, status=status.HTTP_200_OK)
+
         except ReplayMessage.DoesNotExist:
             return Response({
                 'status': 'error',
